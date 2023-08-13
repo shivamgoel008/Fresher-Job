46de4a3971e506c36dabbf115102d9dbe96c6662
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2023-08-14 01:15:23 +0530

    Login Feature added

---
+++ app/src/main/java/com/example/fresherjob/Main/JobListing/JobListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fresherjob.Main.JobListing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fresherjob.R
+
+class JobListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_list)
+    }
+}
